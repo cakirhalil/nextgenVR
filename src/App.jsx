1bb38616e5f9cd5e83a7e49f2833bf976657407b
cakirhalil <halilcakir343@gmail.com>
2024-08-24 21:52:52 +0300

    Fixed import path for WorkFlow component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import Pricing from "./components/Pricing";
 import Testimonials from "./components/Testimonials";
-import Workflow from "./components/WorkFlow";
+import Workflow from "./components/Workflow";
 
 
 const App = () => {
